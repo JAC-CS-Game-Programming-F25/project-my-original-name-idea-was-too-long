26f99c931b048d565ad4fddb61207b8adcdc6b43
AdamLaurin <158082162+AdamLaurin@users.noreply.github.com>
2025-12-09 14:36:02 -0500

    Added game phases to Dice Game to define the gameplay loop and determine what to do when updating the game.

--- src/objects/DiceGame.js
+++ src/objects/DiceGame.js
@@ -1,9 +1,11 @@
 import Input from "../../lib/Input.js";
-import { getRandomNumber, getRandomPositiveNumber } from "../../lib/Random.js";
+import { getRandomPositiveNumber } from "../../lib/Random.js";
 import Character from "../entities/Character.js";
 import Opponent from "../entities/Opponent.js";
 import Direction from "../enums/Direction.js";
-import { CANVAS_HEIGHT, CANVAS_WIDTH, context, input, matter } from "../globals.js";
+import GamePhase from "../enums/GamePhase.js";
+import GameStateName from "../enums/GameStateName.js";
+import { CANVAS_HEIGHT, CANVAS_WIDTH, context, input, stateStack } from "../globals.js";
 import Board from "./Board.js";
 import Die from "./Die.js";
 
@@ -28,28 +30,80 @@ export default class DiceGame {
         this.dice = [new Die(), new Die(), new Die()];
         this.isRolling = false;
 
-        this.wagerAmount = 0;
+        // The wager object to be passed to the Wager State, so that the value set persists across states.
+        this.wagerObject = { wager: 0 }; // if passing a setter works in JS, I might do that instead.
         this.isPlayerTurn = true;
+        this.didPlayerWin = true;
+
+        // The specific phase of the current game.
+        this.gamePhase = GamePhase.Wager;
     }
 
     update(dt) {
-        // Update the dice and determine whether they have finished rolling.
-        this.isRolling = false;
-        this.dice.forEach((die) => {
-            die.update(dt);
-            if (die.isRolling()) this.isRolling = true;
-        });
+        if (this.isRolling) {
+            // Update the dice and determine whether they have finished rolling.
+            this.isRolling = false;
+            this.dice.forEach((die) => {
+                die.update(dt);
+                if (die.isRolling()) this.isRolling = true;
+            });
+            if (this.isRolling) return;
+        }
 
-        // Might be able to do some generic stuff here
-        //if (this.isPlayerTurn) {
+        // Determine what can be done based on which phase of the game you are in.
+        switch (this.gamePhase) {
+            case GamePhase.Wager:
+                // Bring up wager state, pass it the wager object.
+                this.gamePhase = GamePhase.Battle
+                break;
+            case GamePhase.Battle:
+                this.rollBattle()
+                this.gamePhase = GamePhase.BattleRolling;
+                break;
+            case GamePhase.BattleRolling:
+                // Pop up the UI element showing who won the battle.
+
+                this.gamePhase = GamePhase.ToRoll;
+                break;
+            case GamePhase.ToRoll:
+                // Roll right away if it's opponent's turn, or when Space is pressed if it's the player's turn.
+                if (!this.isPlayerTurn ||
+                    (this.isPlayerTurn && input.isKeyPressed(Input.KEYS.SPACE))
+                ) {
+                    this.rollDice();
+                    this.gamePhase = GamePhase.Rolling;
+                }
+                break;
+            case GamePhase.Rolling:
+                // What to do with the roll will depend on the specific game being played.
+                this.checkRoll();
+                break;
+            case GamePhase.Result:
+                // Pop up result UI element based on who won.
+                this.dealOutWinnings();
+                break;
+            case GamePhase.PostGame:
+                if (this.player.isBroke()) {
+                    // If the player has run out of money, they lose!
+                    stateStack.push(GameStateName.GameOver);
+                } else {
+
+                }
+                break;
+        }
+
+
+
+        // Misc testing stuff to remove.
         if (input.isKeyPressed(Input.KEYS.ENTER)) {
             this.rollDice();
         } else if (input.isKeyPressed(Input.KEYS.BACKSLASH)) {
             this.rollBattle();
-        } else if (input.isKeyPressed(Input.KEYS.H)) {
-            // BRING UP HELP SCREEN
         }
-        //}
+    }
+
+    checkRoll() {
+        // Not sure I'll be able to do anything here, might be only implemented in children.
     }
 
     checkVictory() {
@@ -104,6 +158,25 @@ export default class DiceGame {
 
         this.isRolling = true;
         this.isPlayerTurn = this.playerMark > this.opponentMark;
+    }
+
+    /**
+     * Exchange money based on the wager and who won, then determine if the loser is still able to play.
+     * If so, go back to the wager phase, otherwise go to the post-game phase.
+     */
+    dealOutWinnings() {
+        const wager = this.wagerObject.wager;
+        const winner = this.didPlayerWin ? this.player : this.opponent;
+        const loser = this.didPlayerWin ? this.opponent : this.player;
+
+        loser.loseMoney(wager);
+        winner.winMoney(wager);
+
+        if (loser.isBroke()) {
+            this.gamePhase = GamePhase.PostGame;
+        } else {
+            this.gamePhase = GamePhase.Wager;
+        }
     }
 
     render() {
