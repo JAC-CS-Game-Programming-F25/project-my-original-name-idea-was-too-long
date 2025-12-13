6e36daa18f7a33effeaa593ff408b8de28dee3ab
AdamLaurin <158082162+AdamLaurin@users.noreply.github.com>
2025-12-12 21:21:32 -0500

    Added visuals to the Game Over State.

--- src/states/GameOverState.js
+++ src/states/GameOverState.js
@@ -1,17 +1,80 @@
+import Easing from "../../lib/Easing.js";
+import Input from "../../lib/Input.js";
 import State from "../../lib/State.js";
-import { CANVAS_HEIGHT, CANVAS_WIDTH, context } from "../globals.js";
+import { CANVAS_HEIGHT, CANVAS_WIDTH, context, input, stateStack, timer } from "../globals.js";
+import TitleScreenState from "./TitleScreenState.js";
 
 export default class GameOverState extends State {
 	constructor() {
 		super();
+
+		// The visual that will gradually dim the screen when a game over is reached.
+		this.dimmer = 0;
+		this.dimmerFinalAlpha = 0.8;
+		this.fadeDuration = 5;
+		this.returnMessageFadeDuration = 2;
+
+		// The alpha value of the message which will gradually fade into view.
+		this.gameOverMessageAlpha = 0;
+		this.returnMessageAlpha = 0;
+
+		this.gameOverMessage = "You've Lost Everything...";
+		this.returnMessage = "Press ENTER to return to the Title Screen";
+	}
+
+	enter() {
+		// Dim the screen.
+		timer.tween(
+			this,
+			{ dimmer: this.dimmerFinalAlpha },
+			this.fadeDuration,
+			Easing.linear
+		);
+		// Fade in the Game Over message.
+		timer.tween(
+			this,
+			{ gameOverMessageAlpha: 1 },
+			this.fadeDuration,
+			Easing.linear,
+			() => {
+				// Then fade in the return to title screen message.
+				timer.tween(
+					this,
+					{ returnMessageAlpha: 1 },
+					this.returnMessageFadeDuration,
+					Easing.easeOutQuad
+				);
+			}
+		);
+	}
+
+	update(dt) {
+		// Once the Game Over message is visible, let the player go back to the title screen by hitting ENTER.
+		if (this.gameOverMessageAlpha === 1 && input.isKeyPressed(Input.KEYS.ENTER)) {
+			stateStack.clear();
+			stateStack.push(new TitleScreenState);
+		}
 	}
 
 	render() {
 		context.save();
-		context.fillStyle = 'red';
-		context.font = '150px manufacturingConsent';
+		// Render the dimmer.
+		context.fillStyle = 'black';
+		context.globalAlpha = this.dimmer;
+		context.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+		// Render the Game Over Message.
+		context.fillStyle = 'white';
+		context.font = '120px manufacturingConsent';
 		context.textAlign = 'center';
-		context.fillText("Womp Womp!", CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2);
+		context.globalAlpha = this.gameOverMessageAlpha;
+		context.fillText(this.gameOverMessage, CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2);
+
+		// Render the return instructions.
+		context.globalAlpha = this.returnMessageAlpha;
+		context.font = '40px roboto';
+		context.fillText(this.returnMessage, CANVAS_WIDTH / 2, CANVAS_HEIGHT - 120);
+
 		context.restore();
 	}
 }
